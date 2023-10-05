55b61bc9c54f61be0b5c2c1852af86db159cb8f8
FedeCasper <federicorouyere@gmail.com>
2023-10-05 20:41:30 -0300

    Creating dinamic data

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -8,12 +8,33 @@ export const Carrousel = () => {
     fetch("https://mindhub-xj03.onrender.com/api/amazing")
       .then((response) => response.json())
       .then((data) => {
-        let arrayOfTwelve = data.events.slice(0, 12);
+        let arrayOfTwelve = data.events.slice(0, 4);
         setData(arrayOfTwelve);
+
+        function creadoraDeArray(){
+          let slides = ['slide1','slide2','slide3']
+          let indice = 1
+          let array = []
+          let arrayCreado =  slides.map(slide => {
+            array[indice] = []
+            for(let evento of data.events){
+              if(array[indice].length <= 3 && !array[indice].includes(evento)){
+                array[indice].push(evento)
+              }
+            }
+            return array[indice]
+          })
+          return arrayCreado
+        }
+        console.log(creadoraDeArray());
       });
   }
+
+
+  const arraySlide = [1,2,3]
   useEffect(traerDatos, []);
-  console.log(data);
+  console.log(data.length);
+  // console.log(data[0].image);
 
   return (
     <div className="flex w-full p-2 gap-2 border border-red-700 h-[30vh]">
@@ -22,12 +43,11 @@ export const Carrousel = () => {
         className=" text-stone-600 self-center hover:scale-110 hover:text-purple-700 cursor-pointer"
         onClick={() => console.log("Izquierda")}
       />
-      <span className=" inline-grid grid-cols-2 grow gap-2">
-        <div className=" bg-orange-200">Div1</div>
-        <div className=" bg-indigo-300 ">Div2</div>
-        <div className=" bg-fuchsia-200 ">Div3</div>
-        <div className=" bg-teal-100">Div4</div>
-      </span>
+      { arraySlide.map(slide => 
+        <span className=" inline-grid grid-cols-2 grow gap-2" >
+          { data.length > 0 && data.map(evento => <div key={evento.name} style={{backgroundImage: `url('${evento.image}')`}}>Lleno</div>) }
+        </span>
+        ) }
       <ArrowCircleRightIcon
         fontSize="large"
         className=" text-stone-600 self-center hover:scale-110 hover:text-purple-700 cursor-pointer"
