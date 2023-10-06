1a204d14de7725f0126e8913265fe8edcc6e6824
FedeCasper <federicorouyere@gmail.com>
2023-10-06 17:16:33 -0300

    Styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 export const Navbar = () => {
      return (
           <nav className=' flex bg-gradient-to-r from-sky-500 to-indigo-500 gap-8 justify-center py-2'>
-               <a href="#" className=' self-center'>Link 1</a>
-               <a href="#" className=' self-center'>Link 2</a>
+               <a href="#" className=' self-center'>Home</a>
+               <a href="#" className=' self-center'>Cities</a>
                <a href="#" className=' self-center'>Link 3</a>
                <AccountCircleIcon fontSize='medium' className='' />
           </nav>
